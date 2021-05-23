004e8f86c7782a29a5326665515d505d968aac57
John Murphy <jmurphy5613@gmail.com>
2021-05-23 15:14:10 -0400

    added startup message(functional)

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', ()=>{
+    console.log('Bot is online!') 
+})
+
+client.login('ODQ2MDk5Nzk0MDE5MDkwNDg0.YKql1g.t3AhlaC6-XTnyqXJWG8eK-8vKfQ');
