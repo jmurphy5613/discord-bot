a663d89d514f61977683fcaf66007f9b6d002302
John Murphy <jmurphy5613@gmail.com>
2021-05-23 16:21:29 -0400

    bot responds to tournament commands

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const client = new Discord.Client();
 
 const prefix = '-';
 
-client.once('ready', ()=>{
-    console.log('Bot is online!') 
-})
+client.once('ready', () =>{
+    console.log('Bot is online!');
+});
 
-client.on('message', () =>{
-    if(!message.content.startsWith('-') || message.author.bot) return;
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot || !message.author.id === '329062298046169098') return;
 
-    const args = message.content.slice(prefix.length).split(" ");
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'start tournament'){
+    if(command === 'start.tournament'){
         message.channel.send('Tournament started!');
     }
-})
+});
 
-client.login('ODQ2MDk5Nzk0MDE5MDkwNDg0.YKql1g.t3AhlaC6-XTnyqXJWG8eK-8vKfQ');
+client.login('');
